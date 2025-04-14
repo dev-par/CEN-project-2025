63de7dce16bf382f20df89431bd740644bd10226
dev-par <146295367+dev-par@users.noreply.github.com>
2025-04-14 13:52:05 -0400

    Removed unnecessary import and updated docs

--- client/src/components/ClubCard.jsx
+++ client/src/components/ClubCard.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 function ClubCard({ club }) {
   let majors = [];
--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../styles/SideBar.css';
 
 function Sidebar({ filters, setFilter }) {
